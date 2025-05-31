8ebdc3ecd911adf14f8ed8b23a3c4e418b321d78
Dariia K <da23v12@gmail.com>
2025-05-31 11:53:02 +0100

    adding mobile menu

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -9,3 +9,16 @@
 // closeBtnEl.addEventListener('click', e => {
 //   burgerMenuEl.dataset.visible = 'close';
 // });
+
+const openBtn = document.querySelector('[data-action="open"]');
+const closeBtn = document.querySelector('[data-action="close"]');
+const modalMenu = document.querySelector('[data-visible]');
+
+openBtn.addEventListener('click', () => {
+  modalMenu.dataset.visible = 'true';
+});
+
+closeBtn.addEventListener('click', () => {
+  modalMenu.dataset.visible = 'false';
+});
+
