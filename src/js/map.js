05afe80885b6298d743e170c5599eca5b571860a
Dariia K <da23v12@gmail.com>
2025-06-05 23:47:56 +0100

    fixing slider class

--- src/js/map.js
+++ src/js/map.js
@@ -1,3 +1,4 @@
+
 import Swiper from 'swiper';
 import { Navigation, EffectCoverflow } from 'swiper/modules';
 
@@ -8,33 +9,16 @@ import 'swiper/css/effect-coverflow';
 // Ініціалізація Swiper для мобільної карти
 const swiper = new Swiper('.swiper-mob', {
   modules: [Navigation, EffectCoverflow],
-
+  
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 1,
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true, //
+  loop: true,
+
+  spaceBetween: 8, 
 
   navigation: {
     nextEl: '.map-btn-nav-next',
     prevEl: '.map-btn-nav-prev',
   },
-
-  on: {
-    init: () => {
-      fixSwiperGap();
-    },
-    resize: () => {
-      fixSwiperGap();
-    },
-  },
 });
-
-// Фікс візуального gap між слайдами через padding
-function fixSwiperGap() {
-  const slides = document.querySelectorAll('.swiper-mob .swiper-slide');
-  slides.forEach((slide, index) => {
-    slide.style.marginRight = index !== slides.length - 1 ? '8px' : '0';
-  });
-}
